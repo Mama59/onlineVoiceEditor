d01323a6d08ff4e87722a8e793dd09f0909d4c99
Marine Bal <marine.bal.1993@gmail.com>
2017-01-30 12:09:22 +0100

    label ok

--- visualisation/Label.js
+++ visualisation/Label.js
@@ -1,21 +1,38 @@
-Div.js/* This agent exchange position if position is already occuped
+Div.js
+/* This agent exchange position if position is already occuped
  */
 
 class Label extends Agent {
-
+  
   constructor(x, y, env, style, html, opts) {
     if (!style) {
       style = "col-xs-1";
     }
-  
+    
     if (!opts) {
       opts = {
         name: 'test',
-        value : 'test'
+        value: 'test'
       };
     }
-
+    if (!opts.value) {
+      opts.value = 'test';
+    }
+    
     super(x, y, env, style, html, opts);
+    
+    this._html = opts.value;
     this._type = 'label';
   };
+  
+  updateOpts() {
+    super.updateOpts();
+    var self = this.agent;
+    var key = this.key;
+    var value = this.input.value;
+    console.log(value);
+    if (key == 'value') {
+      self._html = value;
+    }
+  }
 }
