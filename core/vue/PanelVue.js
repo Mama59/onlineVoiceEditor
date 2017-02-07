27468b1168dcb8607290f6ffc50c0f7b64e06df7
Junior Buléon <juniorburleon@hotmail.com>
2017-02-07 21:20:19 +0100

    Add petit truc graphique

--- core/vue/PanelVue.js
+++ core/vue/PanelVue.js
@@ -89,7 +89,14 @@ class PanelVue {
 
             form.appendChild(button);
 
+
+            var hr = document.createElement('hr');
+            hr.style=" height: 12px;border: 0;box-shadow: inset 0 12px 12px -12px rgba(0, 0, 0, 0.5);";
+
+            form.appendChild(hr);
+
             detailsSelected.appendChild(form);
+            
         }
     }
 
